afc33aaf78d534c8ed525b9165594fe10544689e
Jean Carlos Holguin <holguinjean1@gmail.com>
2020-07-28 20:46:15 -0400

    Se crea la pantalla de ayuda.\n Se cambia el color de la barra de navegación y los títulos de los formularios.\n Se arreglan algunos estilos generales.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [{ path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }, 
-                        { path: 'result', loadChildren: () => import('./result/result.module').then(m => m.ResultModule) }];
+                        { path: 'result', loadChildren: () => import('./result/result.module').then(m => m.ResultModule) },
+                        { path: 'help', loadChildren: () => import('./help/help.module').then(m => m.HelpModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
